d4c7c0538b32ac2d1681bba25bf77ecc928d77cb
Owen <owenperedo@gmail.com>
2015-11-30 14:23:33 -0400

    fixed rails dependency

--- app/helpers/plugins/camaleon_editor/main_helper.rb
+++ app/helpers/plugins/camaleon_editor/main_helper.rb
@@ -1,5 +1,4 @@
-module Plugins::CamaleonEditor::MainHelper
-  include Plugins::CamaleonEditor::PrivateHelper
+module Plugins::CamaleonEditor::MainHelper extend CamaleonEditorPrivateHelper
   def self.included(klass)
     # klass.helper_method [:my_helper_method] rescue "" # here your methods accessible from views
   end
